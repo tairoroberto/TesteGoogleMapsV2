2d5524856b76fd411890c925a03e64dea7bf92a3
tairoroberto <github@nowsolucoes.com.br>
2015-01-05 20:44:37 -0200

    Adicionado animacao,e trablando com cameraPosition - Tairo

--- app/src/main/java/br/com/tairoroberto/testegooglemapsv2/MainActivity.java
+++ app/src/main/java/br/com/tairoroberto/testegooglemapsv2/MainActivity.java
@@ -5,36 +5,135 @@ import android.support.v4.app.FragmentTransaction;
 import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.LinearLayout;
 
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.GoogleMapOptions;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
 
 
 public class MainActivity extends FragmentActivity {
-
+    private SupportMapFragment mapFragment;
+    private SupportMapFragment mapFragment2;
+    private GoogleMap googleMap;
+    private GoogleMap googleMap2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        LinearLayout layout = (LinearLayout)findViewById(R.id.layout);
+        //MAPS VIA XML
+        //Declara o fragment do maps
+        mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.fragment);
+        googleMap = mapFragment.getMap();
+        //ativa o mapa do xml
+        configmap();
 
+
+        //MAPS VIA API
         //Opção de ordenação do mapa e NavigatorDrawer
         GoogleMapOptions options = new GoogleMapOptions();
         options.zOrderOnTop(true);
 
         //Declara o fragment do maps
-        SupportMapFragment mapFragment = SupportMapFragment.newInstance(options);
+        mapFragment2 = SupportMapFragment.newInstance(options);
 
         //Add o mapFragment ao layout
         FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-        transaction.replace(R.id.layout,mapFragment);
+        transaction.replace(R.id.layout,mapFragment2);
         transaction.commit();
+
+
+
     }
 
+    //o mapa deve ser carregado no onresume
+    @Override
+    protected void onResume() {
+        super.onResume();
+        new Thread(){
+            public void run(){
+                while (mapFragment2.getMap() == null){
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        configmap2();
+                    }
+                });
+
+            }
+        }.start();
+    }
+    public void configmap(){
+        //Se mapFragment não for null, será carregado os googleMap
+        googleMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+
+        //Latitude e logitude
+        LatLng latLng = new LatLng(-23.548998, -46.633058);
+
+        //Configura a posição da camera, local, zoom etc.
+        CameraPosition cameraPosition = new CameraPosition.Builder().target(latLng).zoom(15).bearing(0).tilt(45).build();
+        CameraUpdate cameraUpdate = CameraUpdateFactory.newCameraPosition(cameraPosition);
+
+        //atualiza a posicao da camera no mapa
+        //googleMap.moveCamera(cameraUpdate);
+
+        //animação do mapa
+        googleMap.animateCamera(cameraUpdate, 3000, new GoogleMap.CancelableCallback() {
+            @Override
+            public void onFinish() {
+                Log.i("Script","Animação: CancelableCallback.onFinish()");
+            }
+
+            @Override
+            public void onCancel() {
+                Log.i("Script","Animação: CancelableCallback.onCancel()");
+            }
+        });
+    }
+
+    public void configmap2(){
+        //Se mapFragment não for null, será carregado os googleMap
+        googleMap2 = mapFragment2.getMap();
+        googleMap2.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+
+        //Latitude e logitude
+        LatLng latLng = new LatLng(-23.548998, -46.633058);
+
+        //Configura a posição da camera, local, zoom etc.
+        CameraPosition cameraPosition = new CameraPosition.Builder().target(latLng).zoom(18).bearing(0).tilt(90).build();
+        CameraUpdate cameraUpdate = CameraUpdateFactory.newCameraPosition(cameraPosition);
+
+        //atualiza a posicao da camera no mapa
+        //googleMap.moveCamera(cameraUpdate);
+
+        //animação do mapa
+        googleMap2.animateCamera(cameraUpdate, 3000, new GoogleMap.CancelableCallback() {
+            @Override
+            public void onFinish() {
+                Log.i("Script","Animação: CancelableCallback.onFinish()");
+            }
+
+            @Override
+            public void onCancel() {
+                Log.i("Script","Animação: CancelableCallback.onCancel()");
+            }
+        });
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
